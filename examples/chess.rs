323946609dc5e4a0d4365756583aecd7f0e112d0
SalsaGal <fielddashos@gmail.com>
2023-09-27 18:28:50 +1000

    Add basic chess example

---
+++ examples/chess.rs
@@ -0,0 +1,30 @@
+use glam::vec3;
+use rengine::{renderer::Projection, sprite::Sprite, transform::Transform, Game};
+
+fn main() {
+    rengine::run(Chess);
+}
+
+struct Chess;
+
+impl Game for Chess {
+    fn init(&mut self, data: rengine::GameData) {
+        *data.renderer.projection = Projection::FixedHeight(10.0);
+
+        data.renderer.sprites.extend((0..64).map(|index| {
+            let x = index % 8;
+            let y = index / 8;
+            Sprite::new_quad_color(
+                if (x + y) % 2 == 0 {
+                    wgpu::Color::WHITE
+                } else {
+                    wgpu::Color::BLACK
+                },
+                &[Transform {
+                    translation: vec3(x as f32 - 3.5, y as f32 - 3.5, 0.0),
+                    ..Default::default()
+                }],
+            )
+        }));
+    }
+}
